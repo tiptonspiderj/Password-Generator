ca59c5b6ec549397116d27d4c83d7243a79d81c2
JEREMY TIPTON <32341894+tiptonspiderj@users.noreply.github.com>
2024-08-01 12:59:52 -0400

    Updated JS file and HTML file

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X",
+"Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
+ "=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+ 
+const password1 = document.getElementById("randomPassword1")
+const password2 = document.getElementById("randomPassword2")
+const container = document.getElementsByClassName('container')
 
-let password1 = document.getElementById("randomPassword1")
-let password2 = document.getElementById("randomPassword2")
 let passwordLength = 15
-let password1Text = ""
-let password2Text = ""
+let passwordText = ""
+
+container[0].addEventListener('click', function(e) {
+  if (e.target.id === 'generatePasswords-btn') {
+    generatePasswords()
+    return
+  } else if (e.target.id === 'copyPassword1-btn') {
+    copyPassword(password1)
+    return
+  } else if(e.target.id === 'copyPassword2-btn') {
+    copyPassword(password2)
+  }
+})
 
 function generatePasswords() {
-    password1.value = password(password1Text, passwordLength)
-    password(password2Text, passwordLength)
-    password2.value = password(password2Text, passwordLength)
-    password1Text = ""
-    password2Text = ""
+    password1.value = password(passwordText, passwordLength)
+    passwordText = ""
+    password2.value = password(passwordText, passwordLength)
+    passwordText = ""
 }
 
 function password(textValue, passwordLength) {
@@ -29,14 +43,4 @@ function copyPassword(copiedPassword){
   copiedPassword.setSelectionRange(0, 99999); // For mobile devices
    // Copy the text inside the text field
   navigator.clipboard.writeText(copiedPassword.value);
-  // Alert the copied text
-  alert("Copied the text: " + copiedPassword.value);
-}
-
-function copyPassword1(){
-  copyPassword(password1)
-}
-
-function copyPassword2(){
-  copyPassword(password2)
 }
